ca0a9bedfc1d1f1e884182e39ae1e9ca60cbcf92
krunalraiyani5 <krunalraiyani.itcodeinfotech@gmail.com>
2024-07-19 14:57:48 +0530

    add favicon pack

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,12 +13,38 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
+        <title>Jenis lodaliya - portfolio</title>
+        <meta
+          name="description"
+          content="This is an awesome site built with Next.js"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
         <meta
           name="google-site-verification"
           content="XgWrNxHg2msuxMLcn-dqEvus638STdErY7Ozt3rPJDQ"
         />
-      </head>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
